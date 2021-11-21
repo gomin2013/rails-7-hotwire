22c7de290cdef8cfa440a9926edc9f6ed26ef82b
watthajak2018 <watthajak@abroaders.jp>
2021-11-21 18:47:40 +0700

    Updated i18n configurations.

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,9 @@ module Rails7Hotwire
     config.generators { |g| g.test_framework(:rspec) }
 
     config.active_job.queue_adapter = :sidekiq
+    config.i18n.available_locales = %i[en th]
+    config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :th
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
 end
