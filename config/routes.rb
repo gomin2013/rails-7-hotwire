8c99ecec1373a46367aa99ebd79ec18248cf581d
watthajak2018 <watthajak@abroaders.jp>
2022-02-18 10:09:06 +0700

    Generated room (Scaffold).
    rails g scaffold room name:string;

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :rooms
   if Rails.env.production?
     Sidekiq::Web.use Rack::Auth::Basic do |n, p|
       n == Rails7Hotwire::Application.credentials.basic_auth_sidekiq[:name] &&
